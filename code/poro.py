4f8744bceb0ec4504d4d51f45e068148857c1506
ctypewrier <bmtv33@gmail.com>
2017-05-15 20:01:59 -0700

    Shrunk the arena size, added reward behavior (+5000 on goal, -1 every time tick), creates a poro AI that runs forward.
    TODO:
    
    Implement some algorithms for poro to enable new behavior.
    
    Track trait/fitness pairs of every iteration of poro.
    
    Use top (10) trait/fitness pairs to reproduce new trait pairs, creating a new generation of Poro.

---
+++ code/poro.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+class Poro(object):
+    def __init__(self, traits=[]): # TODO if traits is default, randomize traits.
+        pass
+
+
+
+    def run(self, agent):
+
+        # TODO: Roll random number given the traits, picking an algorithm to run
+
+        agent.sendCommand("move 1")
+
+
+    # TODO: Reproduce method using two trait groups
